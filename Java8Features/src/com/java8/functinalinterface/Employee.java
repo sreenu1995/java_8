ab2d52c108f4fbc40b5b575e7d78af6acbb08361
seenu <sreenu.g95@gmail.com>
2022-03-07 15:34:37 +0530

    Formatted the changes

--- Java8Features/src/com/java8/functinalinterface/Employee.java
+++ Java8Features/src/com/java8/functinalinterface/Employee.java
@@ -1,7 +1,7 @@
 package com.java8.functinalinterface;
 
 public class Employee {
-public static void main(String[] args) {
-	System.out.println("uday");
-}
+	public static void main(String[] args) {
+		System.out.println("uday");
+	}
 }
