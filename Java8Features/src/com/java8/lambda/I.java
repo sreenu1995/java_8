77bada0d4039930f595395dd3800223d56effafd
udaykumar <udaykumartalla@gmail.com>
2022-03-07 15:56:57 +0530

    changes done using interface

---
+++ Java8Features/src/com/java8/lambda/I.java
@@ -0,0 +1,5 @@
+package com.java8.lambda;
+
+public interface I {
+	public void m1();
+}
--- Java8Features/src/com/java8/lambda/Uday.java
+++ Java8Features/src/com/java8/lambda/Uday.java
@@ -1,5 +1,12 @@
 package com.java8.lambda;
 
 public class Uday {
-
+	public static void main(String[] args) {
+	       I j = ()-> {
+			for(int i=0;i<=5;i++) {
+			System.out.println("daimond");
+		}
+	};
+	j.m1();
+  }
 }
